72c48a9bc53185ed3e68bc3f195177c3f3d2bae3
Kağan Ahmet Okan <okaganahmet@gmail.com>
2024-12-21 12:49:46 +0300

    major rework

--- src/script.ts
+++ src/script.ts
@@ -1,112 +1,140 @@
 const canvas = document.getElementById("canvas") as HTMLCanvasElement | null;
 
-if (!canvas) throw new Error("Canvas not found.");
+if (!canvas) throw new Error("Canvas was not found.");
 
-// RESIZE CANVAS TO FIT WINDOW
+const context = canvas.getContext("2d");
+
+if (!context) throw new Error("Canvas is not supported.");
+
+// INITIALIZE CANVAS
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 window.addEventListener("resize", () => {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
+  main();
 });
-
-const context = canvas.getContext("2d");
-
-if (!context) throw new Error("Browser does not support <canvas> element.");
 
 // WALLPAPER ENGINE SETTINGS
 let textColor = "0,255,0";
-let fade = "0.1";
 let backgroundColor = "0,0,0";
-let backgroundRepaint = `0,0,0,${fade}`;
+let fade = 0.1;
 let speed = 30;
+let font = "monospace";
+let fontSize = 16;
+
+let backgroundRepaint = `${backgroundColor},${fade}`;
+
+let timeout: number | null = null;
+let initialTimeout = false;
 
 window.wallpaperPropertyListener = {
   applyUserProperties: (properties) => {
     if (properties.color) {
-      const colors = properties.color.value.split(" ") as string[];
-      const rgb = colors.map(c => Math.ceil(parseFloat(c) * 255)).join(",");
-      textColor = `${rgb}`;
+      textColor = properties.color.value
+        .split(" ")
+        .map((c) => Math.ceil(parseFloat(c) * 255))
+        .join(",");
     }
 
     if (properties.background) {
-      const colors = properties.background.value.split(" ") as string[];
-      const rgb = colors.map(c => Math.ceil(parseFloat(c) * 255)).join(",");
-      backgroundColor = `${rgb}`;
-      backgroundRepaint = `${rgb},${fade}`;
+      backgroundColor = properties.background.value
+        .split(" ")
+        .map((c) => Math.ceil(parseFloat(c) * 255))
+        .join(",");
+      backgroundRepaint = `${backgroundColor},${fade}`;
     }
 
     if (properties.fade) {
-      fade = properties.fade.value.toString();
-      const colors = backgroundRepaint.split(",");
-      colors[3] = fade;
-      backgroundRepaint = colors.join(",");
+      fade = properties.fade.value;
+      backgroundRepaint = `${backgroundColor},${fade}`;
     }
 
     if (properties.speed) {
       speed = properties.speed.value;
-      startDrawing(speed);
+      main();
     }
-  }
-}
 
-// FPS CONTROLS
-let interval: number;
-let now: number;
-let then: number;
-let elapsed: number;
+    if (properties.font) {
+      font = properties.font.value;
+    }
 
-// PAINT CANVAS BLACK
-context.fillStyle = `rgb(${backgroundColor})`;
-context.fillRect(0, 0, canvas.width, canvas.height);
-
-const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890=!?/<>()[]{}";
-const fontSize = 16;
-
-const columns = Math.floor(canvas.width / fontSize);
-const positions = new Array(columns).fill(1);
+    if (properties.size) {
+      fontSize = properties.size.value;
+      main();
+    }
+  },
+};
 
 let frameId: number | null = null;
 
-function draw() {
-  if (!context || !canvas) return;
+function main() {
+  if (!canvas || !context) return;
 
-  requestAnimationFrame(draw);
-
-  now = Date.now();
-  elapsed = now - then;
-
-  if (elapsed < interval) return;
-
-  then = now - (elapsed % interval);
-
-  context.fillStyle = `rgba(${backgroundRepaint})`;
-  context.fillRect(0, 0, canvas.width, canvas.height);
-
-  context.fillStyle = `rgb(${textColor})`;
-  context.font = `${fontSize}px monospace`;
-
-  positions.forEach((position, index) => {
-    const char = chars[Math.floor(Math.random() * chars.length)];
-    context.fillText(char, index * fontSize, position * fontSize);
-
-    if (position * fontSize > canvas.height && Math.random() > 0.975) {
-      positions[index] = 0;
-    }
-
-    positions[index] += 1;
-  });
-}
-
-function startDrawing(fps: number) {
   if (frameId !== null) {
     cancelAnimationFrame(frameId);
+    frameId = null;
   }
 
-  interval = 1000 / fps;
-  then = Date.now();
-  draw();
+  // FPS CONTROLS
+  let interval: number;
+  let now: number;
+  let then: number;
+  let elapsed: number;
+
+  // PAINT CANVAS
+  context.fillStyle = `rgb(${backgroundColor})`;
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  // INITIALIZE ANIMATION
+  const chars =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890=!?/<>()[]{}";
+  const columns = Math.floor(canvas.width / fontSize);
+  const positions = new Array(columns).fill(1);
+
+  function draw() {
+    if (!context || !canvas) return;
+
+    frameId = requestAnimationFrame(draw);
+
+    // CALCULATE FPS
+    now = Date.now();
+    elapsed = now - then;
+    if (elapsed < interval) return;
+
+    then = now - (elapsed % interval);
+
+    context.fillStyle = `rgba(${backgroundRepaint})`;
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    context.fillStyle = `rgb(${textColor})`;
+    context.font = `${fontSize}px ${font}, monospace`;
+
+    positions.forEach((position, index) => {
+      const char = chars[Math.floor(Math.random() * chars.length)];
+      context.fillText(char, index * fontSize, position * fontSize);
+
+      if (position * fontSize > canvas.height && Math.random() > 0.975) {
+        positions[index] = 0;
+      }
+
+      positions[index] += 1;
+    });
+  }
+
+  function start() {
+    if (frameId !== null) {
+      cancelAnimationFrame(frameId);
+      frameId === null;
+    }
+
+    interval = 1000 / speed;
+    then = Date.now();
+    draw();
+  }
+
+  start();
 }
 
-startDrawing(speed);
+main();
--- wallpaper.d.ts
+++ wallpaper.d.ts
@@ -1,7 +1,20 @@
+interface UserProperty<T> {
+  value: T;
+}
+
+interface UserProperties {
+  color?: UserProperty<string>;
+  background?: UserProperty<string>;
+  fade?: UserProperty<number>;
+  speed?: UserProperty<number>;
+  font?: UserProperty<string>;
+  size?: UserProperty<number>;
+}
+
 declare interface Window {
   wallpaperPropertyListener: {
-    applyUserProperties?: (properties: any) => void;
+    applyUserProperties?: (properties: UserProperties) => void;
     setPaused?: (paused: boolean) => void;
     setRate?: (rate: number) => void;
-  }
+  };
 }
